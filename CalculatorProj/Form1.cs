b35e6098274602026bbac20866c0e4267ccdbf02
User <User@WIN-5RSC18PG333>
2021-03-24 14:29:58 +0500

    Добавьте файлы проекта.

---
+++ CalculatorProj/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorProj
+{
+    public partial class Form1 : Form
+    {
+        Double resultValue = 0;
+        string operationPerfomed = "";
+        bool isOperationPerfomed = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            if((textBox_Result.Text == "0") || (isOperationPerfomed))
+            {
+                textBox_Result.Clear();
+            }
+            isOperationPerfomed = false;
+            Button button = (Button)sender;
+            textBox_Result.Text = textBox_Result.Text + button.Text;
+        }
+
+        private void operator_click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            operationPerfomed = button.Text;
+            resultValue = Double.Parse(textBox_Result.Text);
+            labelCurrentOperation.Text = resultValue + " " + operationPerfomed;
+            isOperationPerfomed = true;
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox_Result.Text = "0";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox_Result.Text = "0";
+            resultValue = 0;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            switch(operationPerfomed)
+            {
+                case "+":
+                    textBox_Result.Text = (resultValue + Double.Parse(textBox_Result.Text)).ToString();
+                    break;
+                case "-":
+                    textBox_Result.Text = (resultValue - Double.Parse(textBox_Result.Text)).ToString();
+                    break;
+                case "*":
+                    textBox_Result.Text = (resultValue * Double.Parse(textBox_Result.Text)).ToString();
+                    break;
+                case "/":
+                    textBox_Result.Text = (resultValue / Double.Parse(textBox_Result.Text)).ToString();
+                    break;
+                default:
+                    break;
+
+            }
+        }
+    }
+}
